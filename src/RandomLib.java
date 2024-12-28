1bbfe0c350b47e1309418f0491c60c45e4e40b00
Adebobola <adepojuadebobola6@gmail.com>
2024-12-28 17:33:20 +0100

    Random lib

---
+++ src/RandomLib.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class RandomLib {
+    public static void main(String[] args) {
+        
+        Random random = new Random();
+
+        boolean isHead;
+
+        isHead = random.nextBoolean();
+
+
+        if (isHead) {
+            System.out.println("Is a head");     
+        }
+        else{
+            System.out.println("Is a tail");
+        }
+    }
+}
