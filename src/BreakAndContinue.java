aab21e8f767c9b051166435137c8aa505fed3963
Adebobola <adepojuadebobola6@gmail.com>
2025-01-13 21:43:08 +0100

    Learning Nested Loops.

--- src/BreakAndContinue.java
+++ src/BreakAndContinue.java
@@ -1,15 +1,46 @@
+import java.util.Scanner;
+
 public class BreakAndContinue {
     public static void main(String[] args) {
         // break = break out from a loop (STOP)
         // continue = skip current condition (SKIP)
-    
+        Scanner scanner = new Scanner(System.in);
+        int rows;
+        int columns;
+        char symbol;
 
-        for(int i = 0; i < 10; i++){
-            if(i == 5){
-                continue;
+        System.out.println("Enter number of row: ");
+        rows = scanner.nextInt();
+
+        
+        System.out.println("Enter number of column: ");
+        columns = scanner.nextInt();
+
+        
+        System.out.println("Enter symbol: ");
+        symbol = scanner.next().charAt(0);
+
+
+        for(int i = 0; i < rows; i++){
+            
+            for(int j = 0; j < columns; j++){
+                System.out.print(symbol + " ");
             }
-
-            System.out.print(i + " ");
+            System.out.println();
         }
+
+
+
+        scanner.close();
+
     }
 }
+
+//  for(int i = 0; i < 10; i++){
+//             if(i == 5){
+//                 continue;
+//             }
+
+//             System.out.print(i + " ");
+//         }
+//     }
---
+++ src/NestedLoop.java
@@ -0,0 +1,14 @@
+public class NestedLoop {
+    public static void main(String[] args) {
+       
+    }   
+}
+
+
+ 
+        // for(int i = 1; i<=3; i++){
+        //     System.out.printf("%d)",i);
+        //     for(int j = 1; j <= 9; j++){
+        //         System.out.print("Ramadan Kareem!!! ");
+        //     }
+        // }
