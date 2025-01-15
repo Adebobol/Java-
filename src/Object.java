396f9d3cd2e9fa4ed948165a969714136ae1b00c
Adebobola <adepojuadebobola6@gmail.com>
2025-01-15 04:09:07 +0100

    Learning overloaded constructors

--- src/Object.java
+++ src/Object.java
@@ -4,11 +4,13 @@ class Music {
     int duration;
     boolean listening;
 
+    // overloaded constructors = Allow a class to have multiple constructors with different parameters list
+
     Music(String name,String year, int duration){
         this.name = name;
         this.year = year;
         this.duration = duration;
-        this.listening = true;
+        this.listening = true; 
     }
 
     void play(){
@@ -17,13 +19,32 @@ class Music {
 
 }
 
+class User {
+    String username;
+    String email;
+    int age;
+
+    User(String username){
+        this.username = username;
+        this.email = "Not provided";
+        this.age = 18;
+    }
+
+     User(String username, String email){
+        this.username = username;
+        this.email = email;
+        this.age = 18;
+    }
+}
+
 
 public class Object {
     public static void main(String[] args) {
-        Music music = new Music("ye","2015",4);
-        
+        User user1 = new User("bobola19_", "adepoju@email.com");
+        User user2 = new User("Anifowose");
 
-        music.play();
+        System.out.println(user1.email);
+        System.out.println(user2.email);
 
     }
 }
