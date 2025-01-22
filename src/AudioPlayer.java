1517454e56701ecbc8c886144739cbcd27c6a40a
Adebobola <adepojuadebobola6@gmail.com>
2025-01-22 22:25:05 +0100

    Completed the audio player using java.

--- src/AudioPlayer.java
+++ src/AudioPlayer.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -16,11 +17,41 @@ public class AudioPlayer {
         String filepath = "C:\\Users\\OXYMO\\Java Tut\\main\\src\\audio\\13 Break Or Make Me - (SongsLover.com).wav";
 
         File file = new File(filepath);
+        Scanner scanner = new Scanner(System.in);
 
         try ( AudioInputStream audioStream = AudioSystem.getAudioInputStream(file)) {
 
             Clip clip = AudioSystem.getClip();
             clip.open(audioStream);
+
+
+            String response = "";
+            while(!response.equals("Q")){
+                System.out.println("P = PLay");
+                System.out.println("S = Stop");
+                System.out.println("R = Reset");
+                System.out.println("Q = Quit");
+
+                System.out.print("Enter your choice: ");
+                response = scanner.next().toUpperCase();
+
+                switch(response){
+                    case "P":
+                        clip.start();
+                        break;
+                    case "S":
+                        clip.stop();
+                    case "R":
+                        clip.setMicrosecondPosition(0);
+                        break;
+                    case "Q":
+                        clip.close();
+                        break;
+                    default:
+                        System.out.println("Invalid response.");
+
+                }
+            }
 
            System.out.println("No problems detected.");
         } catch (FileNotFoundException e) {
@@ -33,6 +64,10 @@ public class AudioPlayer {
         } catch (LineUnavailableException e){
             System.out.println("Unable to access audio resource.");
 
+        } finally {
+            System.out.println("Bye!!");
         }
+
+        scanner.close();
     }
 }
